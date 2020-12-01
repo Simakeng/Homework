55a0c32d22b3c6f1d52342ae5653f0f854eaef33
Simakeng <simakeng@outlook.com>
2020-12-01 20:09:06 +0800

    generate make file

--- makefile
+++ makefile
@@ -1,27 +1,2 @@
-hello :
-	@echo "================================"
-	@echo "|  HomeWork Compile Utilities  |"
-	@echo "|          Simakeng            |"
-	@echo "================================"
-	
-.PHONY : hello
-
-python_version := $(shell python -V)
-detect_python:
-ifeq ($(.SHELLSTATUS),0)
-	@echo "[SUCCESS] $(python_version) installed!"
-else
-	@echo [ERROR] Python not install!
-endif
-.PHONY : detect_python
-
-
-
-ltspice_dir = PLEASE CONFIGURE LTSPICE HERE
-detect_ltspice:
-ifeq ($(ltspice_dir),PLEASE CONFIGURE LTSPICE HERE)
-	@echo [ERROR] Please Configure LTSpice install dir!
-else
-	@echo "[SUCCESS] LTSpice installed at $(ltspice_dir)"
-endif
-.PHONY : detect_ltspice
+targit help:
+	echo make c1: Basic N-Channel MOSFET 集成电路CAD 作业1
--- tools/configure.py
+++ tools/configure.py
@@ -43,6 +43,17 @@ def configure():
         f.write(json.dumps(config,ensure_ascii=False))
     pass
 
+def detect_rep_info(path):
+    with open(path,encoding='utf-8') as f:
+        content = f.read()
+    subject = re.search(r'(?<=\$\s*subject\s*:(\s|\n)+).*',content)
+    title = re.search(r'(?<=\$\s*title\s*:(\s|\n)+).*',content)
+    index = re.search(r'(?<=\$\s*index\s*:(\s|\n)+).*',content)
+    subject = subject.group() if subject else ''
+    title = title.group() if title else ''
+    index = index.group() if index else ''
+    return title,subject,index
+
 def find_all_rep_file(root):
     reps = []
     for subdir in os.listdir(root):
@@ -57,11 +68,37 @@ def find_all_rep_file(root):
         if(file.endswith('.rep')):
             reps.append(path.join(root,file))
     return reps
-
+def generate_makefile(reps):
+    with open('makefile','w',encoding='utf-8') as f:
+        f.write('targit help:\n')
+        reps = [detect_rep_info(rep)+(rep,) for rep in reps]
+        ltitle = 0
+        lsubj = 0
+        indexs = {}
+        targits = []
+        for title,subject,index,file_name in reps:
+            if index in indexs.keys():
+                indexs[index] += 1
+            else:
+                indexs[index] = 1
+            index = "%s%d" % (index,indexs[index])
+            ltitle = max(ltitle,len(title))
+            lsubj = max(lsubj,len(subject))
+            targits.append({
+                "title" : title,
+                "subject" : subject,
+                "index" : index,
+                "path" : file_name
+            })
+        for targit in targits:
+            f.write('\techo make %s: %s %s\n' % (targit['index'],targit['title'].ljust(ltitle),targit['subject'].ljust(lsubj)))
+            
 def main():
     if(not path.exists(path.join(root_dir,'config.json'))):
         configure()
     reps = find_all_rep_file(root_dir)
+    
+    generate_makefile(reps)
     print('%d targets successfuly generated.' % len(reps))
     print('type "make help" to view all targets.')
 
